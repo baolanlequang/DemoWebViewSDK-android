47338dcbe389f36d2cb2ad634910255f68cb141f
baolan2005 <baolan2005@gmail.com>
2022-05-17 23:40:11 +0200

    update module

--- DemoWebViewSDK/BLDemoWebSDK/src/main/java/com/baolan2005/bldemowebsdk/MTDMainActivity.java
+++ DemoWebViewSDK/BLDemoWebSDK/src/main/java/com/baolan2005/bldemowebsdk/MTDMainActivity.java
@@ -3,12 +3,22 @@ package com.baolan2005.bldemowebsdk;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MTDMainActivity extends AppCompatActivity {
+
+    private WebView mtdWebView;
+    private WebViewClient mtdViewClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mtdmain);
+
+        mtdWebView = findViewById(R.id.mtdWebview);
+        mtdViewClient = new WebViewClient();
+        mtdWebView.setWebViewClient(mtdViewClient);
+        mtdWebView.loadUrl("https://apple.com");
     }
 }
